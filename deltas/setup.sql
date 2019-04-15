69be15dc66c8932ef12d1dfb07c31ae62076d267
kaustubh <kaustubh51@gmail.com>
2019-04-15 21:37:56 +0530

    Deltas for device logger project.

---
+++ deltas/setup.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE device_logger;
+
+USE device_logger;
+
+CREATE TABLE user (
+    `id` int AUTO_INCREMENT,
+    `name` varchar (50),
+    `user_name` varchar (50),
+    `password` varchar (32),
+    `is_archived` tinyint (1),
+    `is_admin` tinyint (1),
+    PRIMARY KEY (id)
+) ENGINE=MyISAM;
+
+CREATE TABLE device (
+    `id` int AUTO_INCREMENT,
+    `name` varchar (50),
+    `is_archived` tinyint (1),
+    PRIMARY KEY (id)
+) ENGINE=MyISAM;
+
+CREATE TABLE user_device_log (
+    `user_id` int,
+    `device_id` int,
+    `assigned_by` int,
+    `recived_by` int,
+    `comment` varchar (50),
+    `assigned_at` datetime default now(),
+    `recived_at` datetime,
+    `status` tinyint (3) comment "1=assigned, 2=returned",
+    INDEX (`user_id`),
+    INDEX (`device_id`),
+    INDEX (`assigned_by`),
+    INDEX (`recived_by`),
+    INDEX (`assigned_at`),
+    INDEX (`recived_at`),
+    INDEX (`status`)
+) ENGINE=MyISAM;
