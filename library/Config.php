0c18df9958361373879c904a8441e8fc5bcd8371
kaustubh <kaustubh51@gmail.com>
2019-04-27 16:42:31 +0530

    set up application config file.

--- library/Config.php
+++ library/Config.php
@@ -5,19 +5,47 @@
  */
 class Config
 {
+    /**
+     * @var array
+     */
     private $config;
-
-    public function __construct()
-    {
-        $this->config = parse_ini_file(APPLICATION_PATH . '/config/application.ini', true);
-    }
 
     /**
      * @return mixed
      */
+    public function parseIniFile()
+    {
+        $data = parse_ini_file(APPLICATION_PATH . '/config/application.ini', true);
+
+        $config = [];
+        foreach ($data as $section => $configurations) {
+            $explodeSection = explode(':', $section);
+            if (count($explodeSection) > 1) {
+                $config[$explodeSection[0]] = array_merge($data[$explodeSection[1]], $data[$section]);
+                continue;
+            }
+
+            $config[$section] = $configurations;
+        }
+
+        return $config;
+    }
+
+    /**
+     * Get configuaration.
+     *
+     * @return array
+     * @throws Exception
+     */
     public function getConfig()
     {
-        
+        if (null === $this->config) {
+            $this->config = $this->parseIniFile();
+        }
+
+        if (!isset($this->config[APPLICATION_ENV])) {
+            throw new Exception('Environment not set.');
+        }
 
         return $this->config[APPLICATION_ENV];
     }
